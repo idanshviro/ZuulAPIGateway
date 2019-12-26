6217b79d325ee2f046cf6384a0d714d5a5b05dd5
Idan Shviro <idanshviro@gmail.com>
2019-12-26 15:33:39 +0200

    Inital commit

---
+++ src/test/java/microservices/project/ZuulAPIGateway/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.project.ZuulAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
